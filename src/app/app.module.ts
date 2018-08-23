65bf4013181287c9e5e671b7e2989edccef117aa
sm1215 <sm1215@gmail.com>
2018-08-23 15:38:25 -0400

    another batch of work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 
+import { HomePage } from './pages/home.page';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    HomePage,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/home.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
